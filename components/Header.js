14c4d8e062e46ca8b117ed383374717fdd169dac
iamxhunt3r <vikasgupta.92155@gmail.com>
2021-07-03 14:51:32 +0530

    Initial Commit

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+function Header() {
+    return (
+        <header className=" text-center leading-relaxed">
+        <div className="mt-16">
+            <Image
+            className="rounded-full w-40 h-40 block mx-auto shadow-soft"
+            src="https://avatars.githubusercontent.com/u/17361847"
+            width="180"
+            height="180"
+            />
+        </div>
+        <h1 className="text-4xl font-extrabold mt-4 mb-4">Vikas Gupta</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ components/Main.js
@@ -0,0 +1,17 @@
+
+function Main() {
+    return (
+        <section className="text-center">
+            <p className="p-1 font-medium text-xl">Hello, I'm Vikas Gupta. I am Web Developer, Penetration Tester and occassional Bug Bounty Hunter.</p>
+            <p className="p-1 font-medium text-xl">
+            You can contact me 
+            <a 
+            className="text-blue-400 pl-2 hover:text-blue-900"
+            href="mailto:vikasgupta.92155@gmail.com"
+            >Here</a>
+            </p>
+        </section>
+    )
+}
+
+export default Main
