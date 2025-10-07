7392b982e3fe9dd69c7c8bde922eb51737c5dfd6
Vikas <vikas@Vikass-MacBook-Pro.local>
2025-10-07 19:24:31 +0530

    Updated site structure and removed old files

--- pages/api/hello.js
+++
@@ -1,5 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ "Trade Secret 1": 'Maximum 2 trades only' , "Trade Secret 2": 'Target 1:3' , "Trade Secret 3": 'Trade only 3 min and 5 mins timeframe ' , "Trade Secret 4": 'Make Sure you are up for opportunity' })
-}
